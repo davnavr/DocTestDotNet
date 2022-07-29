f93c68a8bc4f40dfeff361ef9344b19dab3c176f
David N <davidcalieln@gmail.com>
2022-07-28 20:02:01 -0700

    Add example project to test running tests

---
+++ example/MyLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace MyLibrary;
+
+public static class Helpers
+{
+    /// <summary>This is a method.</summary>
+    /// <remarks>
+    /// <example>
+    /// This example does something.
+    /// <code testName="HelloExample" testLanguage="csharp">
+    /// System.Console.WriteLine("Hello There!");
+    /// </code>
+    /// </example>
+    /// </remarks>
+    public static void DoSomething()
+    {
+        throw new System.NotImplementedException();
+    }
+}
